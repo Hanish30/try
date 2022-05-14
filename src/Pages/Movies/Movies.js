1c285812fd01a25599c7bea15eb9723d7d804c81
Hanish30 <87928421+Hanish30@users.noreply.github.com>
2022-05-14 14:35:11 +0530

    data changed

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -29,13 +29,14 @@ const Movies = () => {
                 },
     });
     const fetchMovies= async () => {
-        const {data}= await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.React_App_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate&with_genres=${genreforUrl}&with_origin_country=${country}`)
+        const {data}= await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=1cf50e6248dc270629e802686245c2c8&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_watch_monetization_types=flatrate&with_genres=${genreforUrl}&with_origin_country=${country}`)
        // the above line fetches the data from the tmdb api.
        //used to set content as data.results numberOfPages as data.total_pages.
         setContent(data.results);
         setNumberOfPages(data.total_pages);
     }
     useEffect(() => {
+        window.scroll(0,0);
         fetchMovies();
         // eslint-disable-next-line
     }, [page,genreforUrl])
